029aef43af807056d0261aec175fadc26cfeaca1
vyou <vyou@umich.edu>
2023-07-12 12:38:40 -0400

    deleting ZOO.py

--- data prep/zoo_attack.py
+++ data prep/zoo_attack.py
@@ -1,3 +1,11 @@
+from __future__ import print_function
+from cifar10_models.vgg import vgg16_bn, vgg19_bn
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+from torchvision import transforms as T
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
@@ -7,8 +15,10 @@ from numba import jit
 import math
 import time
 import scipy.misc
+import argparse
 import os
 import sys
+import pickle
 from PIL import Image
 #from setup_mnist_model import MNIST
 #from setup_cifar10_model import CIFAR10
@@ -16,6 +26,15 @@ from PIL import Image
 from models.resnet import *
 from cifar10_models.vgg import vgg16_bn, vgg19_bn
 from cifar10_models.resnet import resnet18, resnet34
+
+parser = argparse.ArgumentParser(description='FGSM Attack on CIFAR-10 with VGG Models')
+parser.add_argument('--natural', action='store_true', help='natural prediction on the unperturbed dataset')
+parser.add_argument('--epsilon', default=0.3, type=float, help='epsilon, the maximum amount of perturbation that can be applied')
+parser.add_argument('--model', default='vgg16', help='[vgg16|vgg19|resnet|trades], model that is being attacked')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='disables CUDA training')
+
+args = parser.parse_args()
 
 mean = [0.4914, 0.4822, 0.4465]
 std = [0.2471, 0.2435, 0.2616]
@@ -38,6 +57,11 @@ transform_ = transforms.Compose(
 
 epsilon = 0.3
 samples = 5
+
+# settings
+use_cuda = not args.no_cuda and torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
+kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
 
 """##L2 Black Box Attack"""
 
@@ -439,8 +463,27 @@ def attack_main(model, X_data, Y_data, epsilon_):
 		else:
 			plt.savefig('adam_untargeted_cifar10.png') 
 
-#if __name__=='__main__':
+def main():
 
-	#attack()
+	if args.model == "vgg16":
+		model = vgg16_bn(pretrained=True).to(device)
+	elif args.model == "vgg19":
+		model = vgg19_bn(pretrained=True).to(device)
+	if args.model == "resnet":
+		model = resnet34(pretrained=True).to(device)
+	elif args.model == "TRADES":
+		model = ResNet34().to(device)
+		model.load_state_dict(torch.load("./resnet/model-advres-epoch200.pt", map_location=torch.device('cpu')))
+
+	X_data = np.load("/content/data/ZOO/X.npy")
+	Y_data = np.load("/content/data/ZOO/Y.npy")
+
+	if args.natural:
+		natural(model, X_data, Y_data, epsilon)
+	else:
+		attack_main(model, X_data, Y_data, epsilon)
+
+if __name__ == "__main__":
+	main()
 
 	
